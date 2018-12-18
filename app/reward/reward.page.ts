17a44dd3ecc285f00325adec60b91a4167ab043d
Patton Nguyen <41391357+pnguyen1912@users.noreply.github.com>
2018-12-18 09:11:02 -0600

    Add files via upload

--- app/reward/reward.page.ts
+++ app/reward/reward.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
-
+import {Router} from '@angular/router';
+import {Calendar} from '@ionic-native/calendar/ngx';
 @Component({
   selector: 'app-reward',
   templateUrl: './reward.page.html',
@@ -10,11 +11,15 @@ export class RewardPage implements OnInit {
 
 public FirstName: string;
 
-  constructor(private api:ApiService) { }
+  constructor(public router:Router,
+    private api:ApiService,
+    private calendar: Calendar) { }
 
   ngOnInit() {
   }
-
+  change(){
+    this.router.navigate(['/acharacter'])
+  }
   getdata(){
     const theData = this.api.getMethod('people');
     // this.FirstName = theData[0];
